22411d6b88ad30bdcc53bf7bd0c1c9da6ccb6aa9
Song Zheng <song@hackerville.io>
2017-08-11 23:25:40 -0700

    fix some code

--- app.js
+++ app.js
@@ -5,156 +5,128 @@ var walls = 0;
 var avoided = 0;
 var crash = 0;
 
-function tick()
-{
-    c++;
+// CONSTANTS
+const INTERVAL = 20; // milliseconds
+
+function tick() {
+  c++;
+  document.getElementById('stepsDone').value = c;
+  moveWall(); //move wall to the left
+  checkCollision();
+  experience();
+}
+
+const Game = {
+  interval: '',
+  height: 300,
+  stopSimulation: () => {
+    clearInterval(Game.interval);
+    c = 0;
+    Wall.reset();
+    Car.reset();
     document.getElementById('stepsDone').value = c;
-    moveWall(); //move wall to the left
-    checkCollision();
-    experience();
+    aiPos = 0;
+    walls = 0;
+    crash = 0;
+    avoided = 0;
+  },
+  getZone: (item) => {
+    return item.getCenter() <= 150 ? '0' : '1';
+  }
 }
 
-var tickInterval;
-
-function runSim(state)
-{
-    if(state == 1)//run simulation
-    {
-        if(c > 100)
-        {
-            runSim('0'); //force stop
-        }else{
-                tickInterval = setInterval("tick();", 20);
-             } 
-        
-    }else{
-            //stop simulation
-            clearInterval(tickInterval);
-            c = 0;
-            document.getElementById('stepsDone').value = c;
-            document.getElementById('wall').style.left = null;
-            document.getElementById('wall').style.right = '0px';
-        	document.getElementById('ai').style.marginTop = "50px";
-        	aiPos = 0;
-        	walls = 0;
-        	crash = 0;
-        	avoided = 0;
-        
-         }
+function runSim(state) {
+  if(state == 1) {
+    if(c > 100) {
+      runSim('0'); //force stop
+    } else{
+      Game.interval = setInterval("tick();", INTERVAL);
+    } 
+  } else {
+    Game.stopSimulation();
+  }
 }
-
 
 var lastWallCount = 0;
 var lastAvoided = 0;
 var lastCrash = 0;
 var tryzone = 0;
 
-function experience()
-{
-    var aizone;
-    var wallzone;
-    
-    //get wall and ai zone
-    var getWallY = document.getElementById('wall').offsetTop;
-    var getAIY = document.getElementById('ai').offsetTop;
-    
-    var getWallCenter = (getWallY-100)+50;
-    
-    if(getWallCenter <= 150)
-    {
-    	//zone 0
-      document.getElementById('t_wall_zone').innerHTML = "0";
-      wallzone = "0";
-    }else{
-    			//zone 1
-          document.getElementById('t_wall_zone').innerHTML = "1";
-          wallzone = "1";
-    		}
-        
-    var getAICenter = (getAIY-100)+25;
-    
-    if(getAICenter <= 150)
-    {
-      //zone 0
-      document.getElementById('t_ai_zone').innerHTML = "0";
-      aizone = "0";
-    }else{
-          	//zone 1
-      			document.getElementById('t_ai_zone').innerHTML = "1";
-            aizone = "1";
-    			}
-          
-    //trying
-    document.getElementById('t_trying').innerHTML = tryzone;
-   
+function experience() {
+  var aizone;
+  var wallzone;
 
-   
-    
-    //read from experience 'database'
-    var buildvar = aizone+wallzone+tryzone;
-    var experienceDB = document.getElementById('succ_'+buildvar).innerHTML;
-    
-    //read from 'DB' and decide
-    if(tryzone == 0)
-    {
-    	buildvarOther = aizone+wallzone+1;
-      experienceDBOther = document.getElementById('succ_'+buildvarOther).innerHTML;
-      if(parseInt(experienceDBOther) > parseInt(experienceDB)+parseInt(10))
-      {
-      	buildvar = buildvarOther;
-        experienceDB = document.getElementById('succ_'+buildvar).innerHTML;
-        tryzone = 1;
+  //get wall and ai zone
+  var getWallY = Wall.getY();
+  var getAIY = Car.getY();
+
+  wallzone = Game.getZone(Wall);
+  Stats.setWallZone(wallzone);
+  aizone = Game.getZone(Car);
+  Stats.setCarZone(aizone);
+
+  //trying
+  document.getElementById('t_trying').innerHTML = tryzone;
+
+  //read from experience 'database'
+  var buildvar = aizone+wallzone+tryzone;
+  var experienceDB = document.getElementById('succ_'+buildvar).innerHTML;
+
+  //read from 'DB' and decide
+  if(tryzone == 0) {
+    buildvarOther = aizone+wallzone+1;
+    experienceDBOther = document.getElementById('succ_'+buildvarOther).innerHTML;
+    if(parseInt(experienceDBOther) > parseInt(experienceDB)+parseInt(10)) {
+      buildvar = buildvarOther;
+      experienceDB = document.getElementById('succ_'+buildvar).innerHTML;
+      tryzone = 1;
       }
-    
     }
-    
-    if(tryzone == 1)
-    {
-    	buildvarOther = aizone+wallzone+0;
+
+    if(tryzone == 1) {
+      buildvarOther = aizone+wallzone+0;
       experienceDBOther = document.getElementById('succ_'+buildvarOther).innerHTML;
-      if(parseInt(experienceDBOther) > parseInt(experienceDB)+parseInt(10))
-      {
-      	buildvar = buildvarOther;
+      if(parseInt(experienceDBOther) > parseInt(experienceDB)+parseInt(10)) {
+        buildvar = buildvarOther;
         experienceDB = document.getElementById('succ_'+buildvar).innerHTML;
         tryzone = 0;
       }
-    
-    }
-    
-    
-    //move AI
-   	if(tryzone == 0){moveCar('up');}else{moveCar('down');}
-    
 
-		//update DB only when wall is leftmost
-		if(lastWallCount != walls)
-    {
-    	//do update
-      if(lastAvoided != avoided)
-      {
-      	experienceDB = parseInt(experienceDB)+parseInt(1);
-        document.getElementById('succ_'+buildvar).innerHTML = experienceDB;
-        lastAvoided = avoided;
-      }
-      
-      if(lastCrash != crash)
-      {
-        experienceDB = parseInt(experienceDB)-parseInt(1);
-        document.getElementById('succ_'+buildvar).innerHTML = experienceDB;
-        lastCrash = crash;
-      }
-      
-    	lastWallCount = walls;
-      tryzone = Math.floor(Math.random() * 2);//random between 0 and 1
-      
-        //randomize wall position
-        var randomWallYPos = Math.floor(Math.random() * (200 + 1) + 0);
-        document.getElementById('wall').style.marginTop = randomWallYPos+"px";  
-        
-        document.getElementById('wall').style.left = null;
-        document.getElementById('wall').style.right = '0px';
-      
-      
     }
+
+    //move AI
+    tryzone ? Car.move('down') : Car.move('up');
+
+
+      //update DB only when wall is leftmost
+      if(lastWallCount != walls)
+        {
+          //do update
+          if(lastAvoided != avoided)
+            {
+              experienceDB = parseInt(experienceDB)+parseInt(1);
+              document.getElementById('succ_'+buildvar).innerHTML = experienceDB;
+              lastAvoided = avoided;
+            }
+
+            if(lastCrash != crash)
+              {
+                experienceDB = parseInt(experienceDB)-parseInt(1);
+                document.getElementById('succ_'+buildvar).innerHTML = experienceDB;
+                lastCrash = crash;
+              }
+
+              lastWallCount = walls;
+              tryzone = Math.floor(Math.random() * 2);//random between 0 and 1
+
+              //randomize wall position
+              var randomWallYPos = Math.floor(Math.random() * (200 + 1) + 0);
+              document.getElementById('wall').style.marginTop = randomWallYPos+"px";  
+
+              document.getElementById('wall').style.left = null;
+              document.getElementById('wall').style.right = '0px';
+
+
+        }
 
 }
--- lib.js
+++ lib.js
@@ -1,8 +1,59 @@
 const Car = {
+  element: document.getElementById('ai'),
+  height: 50,
+  width: 100,
   getX: () => {
   },
   getY: () => {
+    return Car.element.offsetTop;
   },
+  getYBottom: () => {
+    return Car.element.offsetTop + Car.height;
+  },
+  move: (direction) => {
+    // boundaries
+    if(aiPos < 50){aiPos = 50;}
+    if(aiPos > 200){aiPos = 200;}
+
+    aiPos = direction == 'down' ? aiPos + 10 : aiPos - 10;
+    
+    Car.element.style.marginTop = aiPos+"px";
+  },
+  getCenter: () => {
+    return Car.getY() - 100 + 25;
+  },
+  reset: () => {
+    Car.element.style.marginTop = "50px";
+  },
+};
+
+const Wall = {
+  width: 100,
+  element: document.getElementById('wall'),
+  getX: () => {
+    return document.getElementById('wall').offsetLeft;
+  },
+  getY: () => {
+    return document.getElementById('wall').offsetTop;
+  },
+  getYBottom: () => {
+    return document.getElementById('wall').offsetTop + Wall.width;
+  },
+  moveLeft: () => {
+    const wallX = Wall.getX();
+    if (wallX <= 0) {
+      Stats.increaseWallCount();
+    } else {
+      document.getElementById('wall').style.left = `${Wall.getX() - 20}px`;
+    }
+  },
+  reset: () => {
+    Wall.element.style.left = null;
+    Wall.element.style.right = '0px';
+  },
+  getCenter: () => {
+    return Wall.getY() - 100 + 50;
+  }
 };
 
 class Sensor {
@@ -14,74 +65,68 @@ class Sensor {
   getRightX() {
     return this.element.offsetLeft+this.width;
   }
+  markDanger() {
+    this.element.style.backgroundColor = 'red';
+  }
+  markSafe() {
+    this.element.style.backgroundColor = 'white';
+  }
 }
 
 const sensors = {
   front: new Sensor('sensor_2'),
 };
 
+const Stats = {
+  getWallCount: () => {
+  },
+  increaseWallCount: () => {
+    walls++;
+  },
+  setWallZone: (val) => {
+    document.getElementById('t_wall_zone').innerHTML = val;
+  },
+  setCarZone: (val) => {
+    document.getElementById('t_ai_zone').innerHTML = val;
+  },
+}
 
-function moveWall()
-{
-    var getWallX = document.getElementById('wall').offsetLeft;
-    var getWallY = document.getElementById('wall').offsetTop;
+
+function moveWall() {
+    var getWallX = Wall.getX();
+    var getWallY = Wall.getY();
     
     var getAIX = sensors.front.getRightX();
-    var getAIY = document.getElementById('ai').offsetTop;
+    var getAIY = Car.getY();
     
     var successRate = Math.floor((avoided/(avoided+crash)*100));   
     
     document.getElementById('topDebug').innerHTML = "&nbsp;["+c+"]<br>&nbsp;Wall ("+getWallX+","+getWallY+")<br>&nbsp;AI ("+getAIX+","+getAIY+")<br>&nbsp;Walls: "+walls+" Avoided: "+avoided+" Crash: "+crash+" Success rate: "+successRate+"%";
         
-    if(getWallX <= 0)
-    {
-        
-        walls++;
-        
-    }else{
-            getWallX = getWallX-20;
-            document.getElementById('wall').style.left = getWallX+'px';
-         }
-    
+    Wall.moveLeft();
 }
 
 // move car up or down, make sure it stays inbound
-function moveCar(direction)
-{
-    //auto correct
-    if(aiPos < 50){aiPos = 50;}
-    if(aiPos > 200){aiPos = 200;}
-    
-    if(direction == 'down')
-    {
-     	aiPos=aiPos+10;   
-    }else{ 
-     		aiPos=aiPos-10;   
-    	 }    
-    
-    
-    document.getElementById('ai').style.marginTop = aiPos+"px";
+function moveCar(direction) {
+  Car.move(direction);
 }
 
 function checkCollision()
 {
-  var getWallX = document.getElementById('wall').offsetLeft;
-  var getAIX = sensors.front.getRightX();
-    
-  var getWallY = document.getElementById('wall').offsetTop+100;
-  var getAIY = document.getElementById('ai').offsetTop;
-        
-    if(getWallX < getAIX && getAIY >= getWallY-100 && getAIY < getWallY || getWallX < getAIX && getWallY-100 > getAIY && getWallY-100 < getAIY+50)
-    {
-     	//moveCar('down');        	 
-        document.getElementById('sensor_2').style.backgroundColor = 'red';
-        //decide if the car was hit
-        if(getWallX < 100){crash++;}
-        
-    }else{
-        	document.getElementById('sensor_2').style.backgroundColor = 'white';
-        	if(getWallX < 100){avoided++;}
-    	 }
-    
+  const frontSensorX = sensors.front.getRightX();
+  const wallX = Wall.getX();
+  const wallY = Wall.getY();
+  const carY = Car.getY();
+  if(wallX < frontSensorX) { // wall is in front sensor range
+    if(carY >= wallY && carY < Wall.getYBottom()
+    || carY < wallY && Car.getYBottom() > wallY) {
+      sensors.front.markDanger();
+      //decide if the car was hit
+      if(wallX < Car.width){crash++;}
+      return;
+    }
+  }
+  sensors.front.markSafe();
+  if(wallX < 100){avoided++;}
 }
 
